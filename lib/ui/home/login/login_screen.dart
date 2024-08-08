f85a6da5ba6bd954a877fe3ca0ea32999edf6a5a
BaselBarakat1 <basel.barakat2003@gmail.com>
2024-08-08 20:39:49 +0300

    + Dialogs

--- lib/ui/home/login/login_screen.dart
+++ lib/ui/home/login/login_screen.dart
@@ -3,32 +3,27 @@ import 'package:flutter/material.dart';
 import 'package:todo_app/firebase_error_codes/firebase_error_codes.dart';
 import 'package:todo_app/ui/home/home_screen.dart';
 import 'package:todo_app/ui/widgets/custom_text_form-field.dart';
+import 'package:todo_app/utils/dialog_utils.dart';
 import 'package:todo_app/utils/email_format.dart';
 import 'package:todo_app/utils/password_format.dart';
 
-class loginScreen extends StatelessWidget {
+class loginScreen extends StatefulWidget {
 static const String routeName = 'login_screen';
+
+  @override
+  State<loginScreen> createState() => _loginScreenState();
+}
+
+class _loginScreenState extends State<loginScreen> {
 TextEditingController emailController = TextEditingController();
+
 TextEditingController passwordController = TextEditingController();
+
 var formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    void login() async {
-     if(formKey.currentState?.validate() == false){
-       return;
-     }
-     try{
-       var credintial = FirebaseAuth.instance.signInWithEmailAndPassword(
-           email: emailController.text, password: passwordController.text);
-       Navigator.pushReplacementNamed(context, HomeScreen.routeName);
-     }on FirebaseAuthException catch (e) {
-       if (e.code == FirebaseErrorCodes.userNotFound) {
-         print('No user found for that email.');
-       } else if (e.code == FirebaseErrorCodes.wrongPassword) {
-         print('Wrong password provided for that user.');
-       }
-     }
-    }
+
     return Container(
       decoration: BoxDecoration(
         image: DecorationImage(image:
@@ -54,6 +49,7 @@ var formKey = GlobalKey<FormState>();
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 customTextFormField(labelText: 'Email',
+                  prefixIcon: Icon(Icons.email),
                   controller: emailController,
                   validator: (input) {
                     if(input == null || input.isEmpty){
@@ -66,6 +62,7 @@ var formKey = GlobalKey<FormState>();
                   },
                 ),
                 customTextFormField(labelText: 'Password',
+                prefixIcon: Icon(Icons.password),
                 controller: passwordController,
                 validator: (input) {
                   if(input == null || input.isEmpty){
@@ -84,7 +81,6 @@ var formKey = GlobalKey<FormState>();
                   style: ElevatedButton.styleFrom(
                       shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(4))),
                     backgroundColor: Color(0xff5D9CEC),
-
                   ),
 
                 )
@@ -95,4 +91,29 @@ var formKey = GlobalKey<FormState>();
       ),
     );
   }
+
+void login() async {
+  if (formKey.currentState?.validate() == false) {
+    return;
+  }
+  try {
+    DialogUtils.showLoadingDialog(context, 'Loading...');
+    var credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: emailController.text,
+        password: passwordController.text
+    );
+    DialogUtils.hideDialog(context);
+    DialogUtils.showMessage(context, 'Logged In Successfully',
+      icon: Icon(Icons.check_circle, color: Colors.green,),
+      postiveActionTitle: 'OK',
+      posAction: () {
+        Navigator.pushReplacementNamed(context, HomeScreen.routeName);
+      },);
+  } on FirebaseAuthException catch (e) {
+    DialogUtils.hideDialog(context);
+    if (e.code == FirebaseErrorCodes.userNotFound || e.code == FirebaseErrorCodes.wrongPassword) {
+      DialogUtils.showMessage(context, 'Wrong Email or Password',postiveActionTitle: 'Ok');
+    }
+  }
+}
 }
--- lib/ui/widgets/custom_text_form-field.dart
+++ lib/ui/widgets/custom_text_form-field.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 
 typedef Validator = String? Function(String?);
 class customTextFormField extends StatelessWidget {
-
+Icon? prefixIcon;
 String labelText ;
 Validator? validator;
 TextEditingController? controller;
-customTextFormField({required this.labelText,this.validator,this.controller});
+customTextFormField({required this.labelText,this.validator,this.controller,this.prefixIcon});
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -16,6 +16,7 @@ customTextFormField({required this.labelText,this.validator,this.controller});
         validator: validator,
         cursorColor: Color(0xff5D9CEC),
         decoration: InputDecoration(
+          prefixIcon: prefixIcon,
           border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(25),
           ),
